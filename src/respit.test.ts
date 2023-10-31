2fc1f87f53b17cf62a360b85c9ddad51740fc184
Max Afanasev <max@afsv.me>
2023-10-31 17:08:11 +0100

    wip: add simplest server

--- src/respit.test.ts
+++ src/respit.test.ts
@@ -1,6 +1,31 @@
-import { test, expect } from "vitest";
-import { respit } from "./respit";
+import { test, expect, vi } from "vitest";
+import { Server } from "./respit";
 
-test("returns truthy", () => {
-  expect(respit()).toBeTruthy();
+test("calls back from one request", () => {
+  const server = new Server();
+  const cb = vi.fn();
+  server.expect("GET", cb);
+  server.handle("GET");
+  expect(cb).toHaveBeenCalled();
+});
+
+test("calls back from two requests", () => {
+  const server = new Server();
+  const getCb = vi.fn();
+  const postCb = vi.fn();
+  server.expect("GET", getCb);
+  server.expect("POST", postCb);
+  server.handle("GET");
+  server.handle("POST");
+  expect(getCb).toHaveBeenCalledOnce();
+  expect(postCb).toHaveBeenCalledOnce();
+});
+
+test("calls back only once", () => {
+  const server = new Server();
+  const cb = vi.fn();
+  server.expect("GET", cb);
+  server.handle("GET");
+  server.handle("GET");
+  expect(cb).toHaveBeenCalledOnce();
 });
--- src/respit.ts
+++ src/respit.ts
@@ -1,3 +1,28 @@
-export function respit(): boolean {
-  return true;
+type Subscriber = () => {};
+
+export class Server {
+  readonly #subscribers = new Map<string, Subscriber[]>();
+
+  expect(request: string, subscriber: Subscriber): void {
+    const queue = this.#subscribers.get(request);
+    if (queue) {
+      queue.push(subscriber);
+    } else {
+      const newQueue = [subscriber];
+      this.#subscribers.set(request, newQueue);
+    }
+  }
+
+  handle(request: string): void {
+    const queue = this.#subscribers.get(request);
+    if (!queue) {
+      return;
+    }
+    while (queue.length > 0) {
+      const subscriber = queue.pop();
+      if (subscriber) {
+        subscriber();
+      }
+    }
+  }
 }
